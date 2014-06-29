984d0fb5a50e47eea5c8f64a1ff89546dcac81b2
ReDetection <redetection@gmail.com>
2014-06-29 10:43:33 +0700

    removed scandit, updated deployment target to 7.0

--- eanshl/Utils/Scanner.mm
+++ eanshl/Utils/Scanner.mm
@@ -7,60 +7,28 @@
 //
 
 #import "Scanner.h"
-#import <ScanditSDK/ScanditSDKBarcodePicker.h>
-#import <ScanditSDK/ScanditSDKOverlayController.h>
-
-static NSString *const APIKEY = @"LNoWHujOEeOEQUAMsRwv1Db9NMx5kHKRf2Rv+jtGJ3g";
 
 static Scanner *sharedInstance;
 
-@interface Scanner () <ScanditSDKOverlayControllerDelegate>
-@property(nonatomic, strong) ScanditSDKBarcodePicker *picker;
+@interface Scanner ()
 @property(nonatomic, copy) void (^completion)(NSString *);
 @end
 
 @implementation Scanner
 
 + (void)initialize {
-    [ScanditSDKBarcodePicker prepareWithAppKey:APIKEY];
     sharedInstance = [[Scanner alloc] init];
 }
 
 - (instancetype)init {
     self = [super init];
     if (self) {
-        self.picker = [[ScanditSDKBarcodePicker alloc] initWithAppKey:APIKEY];
-        self.picker.overlayController.delegate = self;
-        [self.picker.overlayController showSearchBar:YES];
-        [self.picker.overlayController showToolBar:YES];
     }
     return self;
 }
 
 + (void)scanWithCompletion:(void (^)(NSString *code))block fromViewController:(UIViewController *)vc {
     sharedInstance.completion = block;
-    sharedInstance.completion = block;
-    [vc presentViewController:sharedInstance.picker animated:YES completion:^{
-        [sharedInstance.picker startScanning];
-    }];
-}
-
-- (void)scanditSDKOverlayController:(ScanditSDKOverlayController *)overlayController didScanBarcode:(NSDictionary *)barcode {
-    [self.picker stopScanning];
-    [self.picker dismissViewControllerAnimated:YES completion:nil];
-    self.completion(barcode[@"barcode"]);
-}
-
-- (void)scanditSDKOverlayController:(ScanditSDKOverlayController *)overlayController didCancelWithStatus:(NSDictionary *)status {
-    [self.picker stopScanning];
-    [self.picker dismissViewControllerAnimated:YES completion:nil];
-    self.completion(nil);
-}
-
-- (void)scanditSDKOverlayController:(ScanditSDKOverlayController *)overlayController didManualSearch:(NSString *)text {
-    [self.picker stopScanning];
-    [self.picker dismissViewControllerAnimated:YES completion:nil];
-    self.completion(text);
 }
 
 @end
